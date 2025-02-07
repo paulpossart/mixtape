25cdbceac3d2b0061222e540d668e551f05934b7
paul3042 <173724663+paul3042@users.noreply.github.com>
2025-02-07 18:43:51 +0000

    finished searchbar refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Header from './components/1-Header/Header';
-import Searchbar from './components/Searchbar';
+import Searchbar from './components/2-Searchbar/Searchbar';
 import SearchResults from './components/SearchResults';
 import Playlist from './components/Playlist';
 
